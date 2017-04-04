3d7d62e4a04c3bbe75f4e9d01534333056398233
haojie.ma <mhj1137633684@gmail.com>
2017-04-05 06:27:47 +0800

    Add Hello

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.router.draw do
+    root 'application#hello'
+  end
 end
