0e50fda3adcd2db629b480b62444d81fb2390f22
haojie.ma <mhj1137633684@gmail.com>
2017-04-13 04:57:13 +0800

    Finish static pages

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.router.draw do
-    root 'application#hello'
-  end
+  root 'static_pages#home'
 end
